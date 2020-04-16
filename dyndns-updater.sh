32dd88baddc13e7d6bc610dd83f1af0cde8b30f4
Bergregenwurm <technat@technat.ch>
2020-04-16 16:50:35 +0200

    made updater script multidomain capable

--- dyndns-updater.sh
+++ dyndns-updater.sh
@@ -7,15 +7,29 @@
 # - https://tecadmin.net/tutorial/bash/examples/concatenate-string-variables/
 # - https://askubuntu.com/questions/95910/command-for-determining-my-public-ip#95911
 
-########## DynDNS Variables ###########
+###################### General Variables ##################
+dyndns_public_ip=$(curl https://ipinfo.io/ip)
+declare -a dyndns_urls
+
+########## DynDNS Variables for sugus.techpi.ch ###########
 dyndns_domain='sugus.techpi.ch'
 dyndns_user='techpi'
 dyndns_pw='sml12345'
-dyndns_public_ip=$(curl https://ipinfo.io/ip)
-dyndns_post_url="https://${dyndns_user}:${dyndns_pw}@infomaniak.com/nic/update?hostname=${dyndns_domain}&myip=${dyndns_public_ip}"
-#######################################
+dyndns_urls+=("https://${dyndns_user}:${dyndns_pw}@infomaniak.com/nic/update?hostname=${dyndns_domain}&myip=${dyndns_public_ip}")
+###########################################################
 
-######### Update Curl #################
-echo $dyndns_post_url
+########## DynDNS Variables next domain ####### ###########
+#dyndns_domain=''
+#dyndns_user=''
+#dyndns_pw=''
+#dyndns_urls+=("https://${dyndns_user}:${dyndns_pw}@infomaniak.com/nic/update?hostname=${dyndns_domain}&myip=${dyndns_public_ip}")
+###########################################################
 
-echo url=$dyndns_post_url |curl -v --tlsv1.2 -k -K -
+################### Update urls  ##########################
+for  url in "${dyndns_urls[@]}" 
+do
+echo $url
+echo url=$url |curl -v  
+done
+
+
