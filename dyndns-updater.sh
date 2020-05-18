d7fe6b33ff5e81ba01c634c50e2c54cb389ede17
Technat <regenwurm@technat.ch>
2020-05-18 15:46:36 +0200

    Update dyndns-updater.sh

--- dyndns-updater.sh
+++ dyndns-updater.sh
@@ -11,11 +11,11 @@
 dyndns_public_ip=$(curl https://ipinfo.io/ip)
 declare -a dyndns_urls
 
-########## DynDNS Variables for sub.example.com ###########
-dyndns_domain='sub.example.com'
-dyndns_user='techpi'
-dyndns_pw='sml12345'
-dyndns_urls+=("https://${dyndns_user}:${dyndns_pw}@infomaniak.com/nic/update?hostname=${dyndns_domain}&myip=${dyndns_public_ip}")
+########## DynDNS Variables example.com ##################
+#dyndns_domain='sub.example.com'
+#dyndns_user='exampleuser'
+#dyndns_pw='ThisPasswordIsVeryPublic!!!'
+#dyndns_urls+=("https://${dyndns_user}:${dyndns_pw}@infomaniak.com/nic/update?hostname=${dyndns_domain}&myip=${dyndns_public_ip}")
 ###########################################################
 
 ################### Update urls  ##########################
